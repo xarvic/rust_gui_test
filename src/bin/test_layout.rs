725a307b335b8c49dcbf166f5d07471dd4c65759
xarvic <xarvix@web.de>
2021-01-06 20:00:09 +0100

    updated test test_layout.rs

--- src/bin/test_layout.rs
+++ src/bin/test_layout.rs
@@ -1,12 +1,12 @@
-use gui::window::WindowBuilder;
-use gui::widgets::{Widget, WidgetCompose};
 use druid_shell::piet::{Color, Piet, RenderContext};
-use druid_shell::kurbo::{Rect, Size};
-use gui::widget_graph::WidgetContext;
-use gui::event::{Event, EventResponse};
+use druid_shell::kurbo::{Size, Rect};
+use gui::widgets::{Widget, WidgetCompose, Spacer};
+use gui::app::widget_graph::WidgetContext;
+use gui::app::event::{Event, EventResponse, Change};
 use gui::state::key::Key;
-use gui::widgets::layout::{HBox, Container, Spacing, VBox};
-use gui::size::PrefSize;
+use gui::app::size::PrefSize;
+use gui::widgets::layout::{Spacing, HBox, Container, VBox};
+use gui::app::window::WindowBuilder;
 use gui::widgets::text::Label;
 
 struct ColorRect(Color, bool, Size, u32);
@@ -17,13 +17,13 @@ impl ColorRect {
     }
 }
 
-impl Widget<u32> for ColorRect {
-    fn draw(&mut self, painter: &mut Piet, size: Size, _dirty_rect: Rect, _context: WidgetContext, _data: &u32) {
+impl Widget<()> for ColorRect {
+    fn draw(&mut self, painter: &mut Piet, size: Size, _dirty_rect: Rect, _context: WidgetContext, _data: &()) {
         let brush = painter.solid_brush(if self.1 {Color::rgb8(100, 100, 255)} else {self.0.clone()});
         painter.stroke(Rect::new(5.0, 5.0, size.width - 5.0, size.height - 5.0), &brush, 10.0);
     }
 
-    fn handle_event(&mut self, event: Event, _context: WidgetContext, _data: Key<u32>) -> EventResponse{
+    fn handle_event(&mut self, event: Event, _context: WidgetContext, _data: Key<()>) -> EventResponse {
         match event {
             Event::MouseDown(_) => {
                 if !self.1 {
@@ -53,18 +53,22 @@ impl Widget<u32> for ColorRect {
         }
     }
 
-    fn get_pref_size(&mut self, _context: WidgetContext, _data: &u32) -> PrefSize {
+    fn get_pref_size(&mut self, _context: WidgetContext, _data: &()) -> PrefSize {
         PrefSize::min_max(Size::new(50.0, 50.0), Size::new(100.0, 100.0))
     }
 
-    fn layout(&mut self, size: Size, _context: WidgetContext, _data: &u32) {
+    fn layout(&mut self, size: Size, _context: WidgetContext, _data: &()) {
         self.2 = size;
     }
 
     fn build(&mut self, _context: WidgetContext) {}
+
+    fn update(&mut self, new: &(), old: Option<&()>) -> Change {
+        Change::None
+    }
 }
 
-fn test_layout(spacing: Spacing, index: u32) -> impl Widget<u32> {
+fn test_layout(spacing: Spacing, index: u32) -> impl Widget<()> {
      Container::new(HBox::new(spacing, 0.0))
         .child(ColorRect::new(Color::rgb8(180, 0, 0), index + 1))
         .child(
@@ -88,6 +92,7 @@ fn main() {
                 .child(test_layout(Spacing::Padding, 30))
                 .child(Label::new("Spacing::Between", None))
                 .child(test_layout(Spacing::Between, 40))
+                .child(Spacer::grow())
             .padding(20.0)
         )
 }
